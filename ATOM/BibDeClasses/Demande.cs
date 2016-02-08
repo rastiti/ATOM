8a8332c704b67eccf7cc851e2519aab37b560dd9
DiirtyJerk <RServel@laposte.net>
2016-02-08 10:43:14 +0100

    interface

--- ATOM/BibDeClasses/Demande.cs
+++ ATOM/BibDeClasses/Demande.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BibDeClasses
 {
@@ -64,7 +66,7 @@ namespace BibDeClasses
 
         public Demande()
         {
-
+           
         }
 
 
