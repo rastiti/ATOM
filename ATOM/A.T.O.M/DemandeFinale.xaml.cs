2d7d9fcfa398fdc3411689ed38ce8378fbd6cfbb
DiirtyJerk <RServel@laposte.net>
2016-02-08 11:22:02 +0100

    interfaces

---
+++ ATOM/A.T.O.M/DemandeFinale.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace A.T.O.M
+{
+    /// <summary>
+    /// Logique d'interaction pour DemandeFinale.xaml
+    /// </summary>
+    public partial class DemandeFinale : Window
+    {
+        public DemandeFinale()
+        {
+            InitializeComponent();
+        }
+
+        private void Deconnexion(object sender, RoutedEventArgs e)
+        {
+            LoginPage login = new LoginPage();
+            DialogResult = false;
+            Close();
+            login.ShowDialog();
+
+        }
+
+        private void Finaliser(object sender, RoutedEventArgs e)
+        {
+            FinalisationDemande demande = new FinalisationDemande();
+            demande.ShowDialog();
+        }
+        
+    }
+}
