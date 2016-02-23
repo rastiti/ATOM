ba087cb7aef1f21e5bb0af5ac04b37e0f0e080f7
Synallagma <pierreracco15@gmail.com>
2016-02-23 17:19:55 +0100

    DataBaseManager

--- ATOM/BibDeClasses/DataBaseManager.cs
+++ ATOM/BibDeClasses/DataBaseManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,67 @@ using System.Threading.Tasks;
 
 namespace BibDeClasses
 {
-    class DataBaseManager
-    {
+    public class DataBaseManager{
+
+        private static DataBaseManager instance = null;
+        private static MySqlConnection connection;
+        private static string server = "localhost";
+        private static string database = "atom";
+        private static string uid = "root";
+        private static string password = "";
+
+        private DataBaseManager(){
+            try {
+                connection = new MySqlConnection("SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";");
+            }catch(Exception e){
+                Console.WriteLine("Probleme connexion base de données.");
+            }
+        }
+
+        public static DataBaseManager getInstance(){
+            if(instance == null)
+                instance = new DataBaseManager();
+            return instance;
+        }
+
+        public Boolean executerRequete(String requete){
+            if(!OpenConnection())
+                return false;
+            MySqlCommand cmd = new MySqlCommand(requete, connection);
+            cmd.ExecuteNonQuery();
+            if (!CloseConnection())
+                return false;
+            return true;
+        }
+
+        private static bool OpenConnection(){
+            try{
+                connection.Open();
+                return true;
+            }catch (MySqlException ex){
+                switch (ex.Number){
+                    case 0:
+                        //0: Cannot connect to server.
+                        Console.WriteLine("Connexion au serveur impossible.");
+                    break;
+
+                    case 1045:
+                        //1045: Invalid user name and/or password.
+                        Console.WriteLine("Connexion au serveur impossible.");
+                    break;
+                }
+                return false;
+            }
+        }
+
+        private static bool CloseConnection(){
+            try{
+                connection.Close();
+                return true;
+            }catch (MySqlException e){
+                Console.WriteLine("Probleme fermeture connexion base de données.");
+                return false;
+            }
+        }
     }
 }
