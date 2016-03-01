e53194830cb35209439a2213389e175b0e82d376
Synallagma <pierreracco15@gmail.com>
2016-03-01 20:49:11 +0100

    Connexion

--- ATOM/A.T.O.M/LoginPage.xaml.cs
+++ ATOM/A.T.O.M/LoginPage.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using BibDeClasses;
-
 namespace ATOM
 {
     /// <summary>
@@ -33,33 +32,45 @@ namespace ATOM
 
         private void Valider(object sender, RoutedEventArgs e)
         {
-
-
             DataBaseManager dbm = DataBaseManager.getInstance();
             String log = login.Text;
             String pass = password.Password;
-            Console.WriteLine("login: " + log + " Password: " + pass);
-            String query = ("SELECT * FROM utilisateur WHERE Login='" + log + "' AND Mdp='" + pass + "';");
-            Console.WriteLine(query);
+            String query = "SELECT * FROM utilisateur WHERE Login='" + log + "' AND Mdp='" + pass + "';";
+            //Console.WriteLine("login: " + log + " Password: " + pass);
+            //Console.WriteLine("SELECT * FROM utilisateur WHERE Login=" + log + " AND Mdp=" + pass + ";");
             var reader = dbm.executerRequete(query);
             Console.WriteLine(reader);
-
-            if (reader == true)
-            {
-                //test pour savoir si la personne a accès a quelle fenetre
-
-                //if(l'utilisateur est régulateur ou du service de soin )
-                DemandeATraiter demande = new DemandeATraiter();
-                Close();
-                demande.ShowDialog();
+            if (reader is Boolean)
+                MessageBox.Show("Identifiant ou mot de passe incorrect");
+            else if(reader is String)
+                MessageBox.Show(reader);
+            else {
+                switch((String)reader[6]){
+                    case "regulateur":
+                        DemandeATraiter dat = new DemandeATraiter();
+                        Close();
+                        dat.ShowDialog();
+                        break;
+                    case "soin":
+                        DemandeEnCours dec = new DemandeEnCours();
+                        Close();
+                        dec.ShowDialog();
+                        break;
+                    case "ambulancier":
+                        DemandeFinale df = new DemandeFinale();
+                        Close();
+                        df.ShowDialog();
+                        break;
+                    case "admin":
+                        //en attente fenetre création compte
+                        //Close();
+                        //demande.ShowDialog();
+                        break;
+                    default:
+                        MessageBox.Show("Problème avec votre compte. Veuillez contacter l'administrateur.");
+                        break;
+                }
             }
-            else
-            {
-                MessageBox.Show("Identifiants incorrects");
-            }
-
-
-
         }
     }
 }
--- ATOM/BibDeClasses/DataBaseManager.cs
+++ ATOM/BibDeClasses/DataBaseManager.cs
@@ -5,10 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BibDeClasses
-{
-    public class DataBaseManager
-    {
+namespace BibDeClasses{
+    public class DataBaseManager{
 
         private static DataBaseManager instance = null;
         private static MySqlConnection connection;
@@ -17,81 +15,73 @@ namespace BibDeClasses
         private static string uid = "root";
         private static string password = "";
 
-        private DataBaseManager()
-        {
-            try
-            {
+        private DataBaseManager(){
+            try {
                 connection = new MySqlConnection("SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";");
-            }
-            catch (Exception e)
-            {
+            }catch(Exception e){
                 Console.WriteLine("Probleme connexion base de données.");
             }
         }
 
-        public static DataBaseManager getInstance()
-        {
-            if (instance == null)
+        public static DataBaseManager getInstance(){
+            if(instance == null)
                 instance = new DataBaseManager();
             return instance;
         }
 
-        public dynamic executerRequete(String requete)
-        {
+        public dynamic executerRequete(String requete){
+            /*
+                SI INSERT -> True ou False en fonction du résultat
+                SI SELECT -> False(erreur login/mdp) ou String(message erreur) ou tableau de données
+                SI DELETE -> False ou False
+            */
             if (!OpenConnection())
-                return false;
+                return "Application indisponible";
             MySqlCommand cmd = new MySqlCommand(requete, connection);
-            cmd.ExecuteNonQuery();
-
-            MySqlDataReader myReader = cmd.ExecuteReader();
-            if (!myReader.Read())
-            {
-                if (!CloseConnection())
+            MySqlDataReader rdr = cmd.ExecuteReader();
+            try {
+                if (!rdr.HasRows) {
                     return false;
-                return false;
+                }
+                Object[] tab = new Object[rdr.FieldCount];
+                while (rdr.Read()){
+                    int count = rdr.GetValues(tab);
+                    for (int i = 0; i < count; i++)
+                        Console.WriteLine(tab[i]);
+                }
+                return tab;
             }
-            else {
-                if (!CloseConnection())
-                    return false;
+            finally{
+                CloseConnection();
+                rdr.Close();
             }
-            myReader.Close();
-            return true;
         }
 
-        public bool OpenConnection()
-        {
-            try
-            {
+        private static bool OpenConnection(){
+            try{
                 connection.Open();
                 return true;
-            }
-            catch (MySqlException ex)
-            {
-                switch (ex.Number)
-                {
+            }catch(MySqlException ex){
+                switch (ex.Number){
                     case 0:
                         //0: Cannot connect to server.
                         Console.WriteLine("Connexion au serveur impossible.");
-                        break;
+                    break;
 
                     case 1045:
                         //1045: Invalid user name and/or password.
                         Console.WriteLine("Connexion au serveur impossible.");
-                        break;
+                    break;
                 }
                 return false;
             }
         }
 
-        private static bool CloseConnection()
-        {
-            try
-            {
+        private static bool CloseConnection(){
+            try{
                 connection.Close();
                 return true;
-            }
-            catch (MySqlException e)
-            {
+            }catch(MySqlException e){
                 Console.WriteLine("Probleme fermeture connexion base de données.");
                 return false;
             }
